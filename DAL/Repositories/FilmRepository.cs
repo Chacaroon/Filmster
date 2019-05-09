f8af60828eb363e9474861b619f8a8b5f34c51b5
Chacaroon <nekit2326@gmail.com>
2019-05-09 23:59:55 +0300

    Include related data in FilmRepository

--- DAL/Repositories/FilmRepository.cs
+++ DAL/Repositories/FilmRepository.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 using SharedKernel.Abstractions.DAL.Repositories;
 
 namespace DAL.Repositories
 {
 	public class FilmRepository : Repository<Film>
 	{
-		public FilmRepository(ApplicationContext dbContext) 
+		public FilmRepository(ApplicationContext dbContext)
 			: base(dbContext)
 		{
 		}
+
+		public override IQueryable<Film> GetAll() =>
+			base.GetAll()
+			    .Include(f => f.FilmGenres)
+			    .ThenInclude(fg => fg.Genre)
+			    .Include(f => f.FilmActors)
+			    .ThenInclude(fa => fa.Actor)
+			    .Include(f => f.FilmProducers)
+			    .ThenInclude(fp => fp.Producer);
 	}
 }
