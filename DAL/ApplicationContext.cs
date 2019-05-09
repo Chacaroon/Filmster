263cec6d4e3acc43b6025ba85f6f6e993f72a405
Chacaroon <nekit2326@gmail.com>
2019-05-09 14:30:27 +0300

    Refactor relationships

--- DAL/ApplicationContext.cs
+++ DAL/ApplicationContext.cs
@@ -30,39 +30,6 @@ namespace DAL
 			modelBuilder.Entity<FilmProducer>()
 				.HasKey(fp => new { fp.FilmId, fp.ProducerId });
 
-
-			modelBuilder.Entity<FilmGenre>()
-				.HasOne(fg => fg.Film)
-				.WithMany(f => f.FilmGenres)
-				.HasForeignKey(fg => fg.FilmId);
-
-			modelBuilder.Entity<FilmGenre>()
-				.HasOne(fg => fg.Genre)
-				.WithMany(g => g.FilmGenres)
-				.HasForeignKey(fg => fg.GenreId);
-
-
-			modelBuilder.Entity<FilmActor>()
-				.HasOne(fa => fa.Actor)
-				.WithMany(a => a.FilmActors)
-				.HasForeignKey(fa => fa.ActorId);
-
-			modelBuilder.Entity<FilmActor>()
-				.HasOne(fa => fa.Film)
-				.WithMany(f => f.FilmActors)
-				.HasForeignKey(fa => fa.FilmId);
-
-
-			modelBuilder.Entity<FilmProducer>()
-				.HasOne(fp => fp.Film)
-				.WithMany(f => f.FilmProducers)
-				.HasForeignKey(fp => fp.FilmId);
-
-			modelBuilder.Entity<FilmProducer>()
-				.HasOne(fp => fp.Producer)
-				.WithMany(p => p.FilmProducers)
-				.HasForeignKey(fp => fp.ProducerId);
-
 			#endregion
 
 			#region DataSeeding
